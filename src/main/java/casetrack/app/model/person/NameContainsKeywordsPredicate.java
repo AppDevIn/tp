a8e0bab29e09f34370d22716b1919f1c80d7738f
Jeya <e1356515@u.nus.edu>
2025-11-01 13:28:23 +0800

    Fix name logic accept prefix searcgh

--- src/main/java/casetrack/app/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/casetrack/app/model/person/NameContainsKeywordsPredicate.java
@@ -1,6 +1,5 @@
 package casetrack.app.model.person;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
@@ -18,11 +17,15 @@ public class NameContainsKeywordsPredicate implements Predicate<Person> {
 
     @Override
     public boolean test(Person person) {
-        if (keywords.isEmpty()) return false;
+        if (keywords.isEmpty()) {
+            return false;
+        }
         String[] nameWords = person.getName().fullName.split("\\s+");
         outer: for (int i = 0; i <= nameWords.length - keywords.size(); i++) {
             for (int j = 0; j < keywords.size(); j++) {
-                if (!nameWords[i + j].toLowerCase().startsWith(keywords.get(j).toLowerCase())) continue outer;
+                if (!nameWords[i + j].toLowerCase().startsWith(keywords.get(j).toLowerCase())) {
+                    continue outer;
+                }
             }
             return true;
         }
